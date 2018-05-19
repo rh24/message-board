1f3bbd3737bdc6f2f41e7b6fb583bf69283246b6
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-19 15:01:17 -0400

    Add Reply model and associations to User and Post models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180519175524) do
+ActiveRecord::Schema.define(version: 20180519185848) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "date_published"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
